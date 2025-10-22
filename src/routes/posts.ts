dd57fd7ea73ebf9403287cc82c9e3af6c94119c8
Jeferson Lugo <jefersoneduardoluego@gmail.com>
2025-10-22 18:55:54 -0400

    get single-post route changed

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -20,7 +20,7 @@ const router = Router()
 
 router.post('/', authenticate, upload.single('media'), postPost)
 router.get('/', getPosts)
-router.get('post/:id', getSinglePost)
+router.get('/single-post/:id', getSinglePost)
 router.get('/me', authenticate, getOwnPosts)
 router.get('/people/:username', getPublicPosts)
 router.patch('/:id', authenticate, editPost)
